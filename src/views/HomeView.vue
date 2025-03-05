e4f114fede62101c05ef0e479c2171b167901df7
Valentin Björkli Castillo <valentin.bjorkli@outlook.com>
2025-03-05 16:00:06 +0100

    add secondary button and styling
    Co-Authored-By: Matilda <183352340+matildasoderhall@users.noreply.github.com>
    Co-Authored-By: Mandis <amandawiktorsson@me.com>

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import PrimaryButton from '@/components/atoms/PrimaryButton.vue';
+import SecondaryButton from '@/components/atoms/SecondaryButton.vue';
 </script>
 
 <template>
 	<h1>Home</h1>
 	<PrimaryButton />
+	<SecondaryButton />
 </template>
 
 <style lang="scss" scoped>
