09b4510a78767fbe0a3ea530ee689d2b9f1b816c
Valentin Björkli Castillo <valentin.bjorkli@outlook.com>
2025-03-05 15:56:35 +0100

    add styling to primary button
    Co-Authored-By: Matilda <183352340+matildasoderhall@users.noreply.github.com>
    Co-Authored-By: Mandis <amandawiktorsson@me.com>

--- src/components/atoms/PrimaryButton.vue
+++ src/components/atoms/PrimaryButton.vue
@@ -6,5 +6,28 @@
 </template>
 
 <style lang="scss" scoped>
+button {
+    background-color: $forest-color;
+    color: $mustard-color;
+    border: solid 3px $mustard-color;
+    border-radius: $button-border-radius;
+    padding-inline: 2.5rem;
+    font-family: $primary-font;
+    font-size: $h4-fontsize;
+    text-transform: uppercase;
+    filter: drop-shadow(rgba(0, 0, 0, 0.25) -4px 4px 4px);
+}
+
+button:hover {
+    background-color: $forest-color;
+    color: $aged-copper-color;
+    border: solid 3px $aged-copper-color;
+}
+
+button:active {
+    background-color: $mustard-color;
+    color: $forest-color;
+    border: solid 3px $forest-color;
+}
 
 </style>
