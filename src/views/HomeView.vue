b98997b035e940c81f6a053f3c397d8d03c77c16
Valentin Björkli Castillo <valentin.bjorkli@outlook.com>
2025-03-06 11:53:55 +0100

    add styling and mobile layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import SecondaryButton from '@/components/atoms/SecondaryButton.vue';
+import TertiaryButton from '@/components/atoms/TertiaryButton.vue';
 import SmallCard from '@/components/SmallCard.vue';
 </script>
 
@@ -7,20 +7,20 @@ import SmallCard from '@/components/SmallCard.vue';
 	<div class="home-view">
 		<figure>
 			<img
-				src="@/assets/images/reptilicious-logo.png"
+				src="@/assets/images/reptilicious_logo.png"
 				width="372"
 				height="326"
 				loading="lazy"
 				alt="a waving green crocodile and a green snake that looks happy below a text that says retilicious"
 			/>
 		</figure>
-		<section>
+		<section class="heading-one-container">
 			<h1>Welcome Reptiles</h1>
 			<span>Explore a World of Flavors</span>
 		</section>
-		<div>
+		<div class="content-container">
 			<!-- <Carousel /> -->
-			<SecondaryButton />
+			<TertiaryButton />
 			<!-- TODO: Add props to change label for button to: "All flavors" -->
 			<SmallCard cardHeader="Reptilicious" cardSubheader="Wildly Delicious" cardContent="Unleash your inner adventurer with Reptilicious! Our jungle-inspired candies deliver bold, unforgettable flavors that take you on a wild ride. Whether you crave sweetness, saltiness, or sourness, every bite is a journey into the heart of the wild. Get ready to snack like never before!" />
 		</div>
@@ -29,9 +29,38 @@ import SmallCard from '@/components/SmallCard.vue';
 
 <style lang="scss" scoped>
 
-h1 {
-	font-family: $primary-font;
-	font-size: $h1-fontsize;
+.home-view {
+	min-width: 100vw;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	.heading-one-container {
+		min-width: 100%;
+		background-color: $white-color;
+		text-align: center;
+		color: $forest-color;
+		padding-top: $small-margin;
+		padding-bottom: $small-margin;
+		h1 {
+			font-family: $primary-font;
+			font-size: $h3-fontsize;
+			text-transform: uppercase;
+			margin: 0; //TODO: Remove this one after adding CSS-normalize
+			margin-bottom: 0.5rem;
+		}
+		span {
+			font-family: $secondary-font;
+			font-size: $subhead-fontsize;
+		}
+	}
+	.content-container {
+		margin-top: $large-margin;
+		margin-bottom: $large-margin;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: $large-margin;
+	}
 }
 
 </style>
