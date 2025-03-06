f39bcf496cae04e0233e0e7a153efa0ab51c67c8
Valentin Björkli Castillo <valentin.bjorkli@outlook.com>
2025-03-06 10:31:04 +0100

    add small card component basics
    basic structure and style

---
+++ src/components/SmallCard.vue
@@ -0,0 +1,36 @@
+<template>
+    <article>
+        <h2>Rubrik</h2>
+        <span class="subheading">Subheading</span>
+        <p>Text</p>
+    </article>
+</template>
+
+<style lang="scss" scoped>
+article {
+    background-color: $white-color;
+    color: $forest-color;
+    border-radius: $small-card-border-radius;
+    display: flex;
+    flex-direction: column;
+    gap: 0.25rem;
+    padding: 1.25rem;
+    filter: drop-shadow(rgba(0, 0, 0, 0.25) -4px 4px 4px);
+    h2 {
+        font-family: $primary-font;
+        font-size: $h3-fontsize;
+        text-transform: uppercase;
+        margin: 0;
+    }
+    .subheading {
+        font-family: $secondary-font;
+        font-size: $subhead-fontsize;
+        margin: 0;
+    }
+    p {
+        font-family: $secondary-font;
+        font-size: $body-fontsize;
+        margin: 0;
+    }
+}
+</style>
