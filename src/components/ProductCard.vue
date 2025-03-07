9fdf9eb92485553c7474d24d5eed609c3fa27800
matildasoderhall <soderhall.matilda@gmail.com>
2025-03-07 10:40:48 +0100

    add define props

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+const props = defineProps({
+	cardHeader: { type: String, required: true },
+	cardSubheader: { type: String, required: true },
+	cardContent: { type: String, required: true },
+	imgSrc: { type: String, required: true },
+	imgAlt: { type: String, required: true },
+});
+</script>
+
+<template>
+	<article>
+		<div class="img-container">
+			<img src="{{ imgSrc }}" alt="{{ imgAlt }}" width="256" height="261" />
+			<h2>{{ cardHeader }}</h2>
+		</div>
+	</article>
+</template>
