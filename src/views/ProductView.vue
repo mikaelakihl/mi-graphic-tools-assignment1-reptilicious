f2bc3ef0ab8c342f73ab9b55b70ef9ef73b83bf4
matildasoderhall <soderhall.matilda@gmail.com>
2025-03-07 14:32:37 +0100

    add smallCard and generel styling

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,32 +1,53 @@
 <script lang="ts" setup>
 import ProductCard from '../components/ProductCard.vue';
+import SmallCard from '../components/SmallCard.vue';
 import krokodileImageGreen from '../assets/images/krokodil_kobra_green.jpg';
 import krokodileImageBlue from '../assets/images/krokodil_kobra_blue.jpg';
 import krokodileImageRed from '../assets/images/krokodil_kobra_red.png';
 </script>
 
 <template>
-	<h1>Products</h1>
-
-	<ProductCard
-		:imgSrc="krokodileImageGreen"
-		:imgAlt="'A bunch of happy reptiles in a green jungle'"
-		:cardHeader="'Cuban Crocodiles'"
-		:cardSubheader="'Sweet'"
-		:cardContent="'Our famous cola flavored crocodiles!'"
-	/>
-	<ProductCard
-		:imgSrc="krokodileImageRed"
-		:imgAlt="'A bunch of happy reptiles in a red jungle'"
-		:cardHeader="'King Cobras'"
-		:cardSubheader="'Sour'"
-		:cardContent="'Nothing beats the king!'"
-	/>
-	<ProductCard
-		:imgSrc="krokodileImageBlue"
-		:imgAlt="'A bunch of happy reptiles in a blue jungle'"
-		:cardHeader="'Lizard Sizzle'"
-		:cardSubheader="'Salty'"
-		:cardContent="'These lizards are sizzling!'"
-	/>
+	<main>
+		<h1>Products</h1>
+		<SmallCard
+			:cardHeader="'Taste the jungle'"
+			:cardSubheader="'Here you can find all our crazy flavors!'"
+			:cardContent="''"
+		/>
+		<ProductCard
+			:imgSrc="krokodileImageGreen"
+			:imgAlt="'A bunch of happy reptiles in a green jungle'"
+			:cardHeader="'Cuban Crocodiles'"
+			:cardSubheader="'Sweet'"
+			:cardContent="'Our famous cola flavored crocodiles!'"
+		/>
+		<ProductCard
+			:imgSrc="krokodileImageRed"
+			:imgAlt="'A bunch of happy reptiles in a red jungle'"
+			:cardHeader="'King Cobras'"
+			:cardSubheader="'Sour'"
+			:cardContent="'Nothing beats the king!'"
+		/>
+		<ProductCard
+			:imgSrc="krokodileImageBlue"
+			:imgAlt="'A bunch of happy reptiles in a blue jungle'"
+			:cardHeader="'Lizard Sizzle'"
+			:cardSubheader="'Salty'"
+			:cardContent="'These lizards are sizzling!'"
+		/>
+	</main>
 </template>
+
+<style lang="scss" scoped>
+main {
+	margin: $small-margin;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+h1 {
+	font-family: $primary-font;
+	font-size: $h1-fontsize;
+	color: $mustard-color;
+}
+</style>
