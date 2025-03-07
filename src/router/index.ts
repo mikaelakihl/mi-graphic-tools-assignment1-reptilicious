a6563bef95010d43b289d22de8d6d6d5f5f07944
Omar Alawi <omar.alawi17@gmail.com>
2025-03-07 09:00:48 +0100

    📨 include env and assets in tsconfig

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,7 +17,7 @@ const router = createRouter({
 			// which is lazy-loaded when the route is visited.
 			component: () => import('../views/AboutView.vue'),
 		},
-    {
+		{
 			path: '/product',
 			name: 'product',
 			component: () => import('../views/ProductView.vue'),
