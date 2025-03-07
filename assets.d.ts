c3326c752eee2995c9da12d1de73ef24cbcd878e
Omar Alawi <omar.alawi17@gmail.com>
2025-03-07 09:34:42 +0100

    declare vue components

--- assets.d.ts
+++ assets.d.ts
@@ -12,3 +12,9 @@ declare module '*.svg' {
 	const value: string;
 	export default value;
 }
+
+declare module '*.vue' {
+	import { DefineComponent } from 'vue';
+	const component: DefineComponent<{}, {}, any>;
+	export default component;
+}
