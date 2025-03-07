9bb04668f981baf310722d157fbc99f45d645adc
amandawiktorsson <amandawiktorsson@me.com>
2025-03-07 14:26:55 +0100

    hide logo on homepage

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+import HeaderMobile from '../fixtures/HeaderMobile.vue';
+</script>
+
 <template>
+	<HeaderMobile />
+
 	<h1>About</h1>
 </template>
--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+import HeaderMobile from '../fixtures/HeaderMobile.vue';
+</script>
+
 <template>
+	<HeaderMobile />
+
 	<h1>Products</h1>
 </template>
