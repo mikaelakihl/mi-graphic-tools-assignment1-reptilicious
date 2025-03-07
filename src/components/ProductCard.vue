a964af98eff2501f70ee379eb83906b0eafc9097
matildasoderhall <soderhall.matilda@gmail.com>
2025-03-07 11:48:59 +0100

    add basic html to card

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,19 +1,26 @@
 <script lang="ts" setup>
 import { defineProps } from 'vue';
-const props = defineProps({
-	cardHeader: { type: String, required: true },
-	cardSubheader: { type: String, required: true },
-	cardContent: { type: String, required: true },
-	imgSrc: { type: String, required: true },
-	imgAlt: { type: String, required: true },
-});
+import PrimaryButton from '../components/atoms/PrimaryButton.vue';
+
+const props = defineProps<{
+	cardHeader: string;
+	cardSubheader: string;
+	cardContent: string;
+	imgSrc: string;
+	imgAlt: string;
+}>();
 </script>
 
 <template>
 	<article>
 		<div class="img-container">
-			<img src="{{ imgSrc }}" alt="{{ imgAlt }}" width="256" height="261" />
-			<h2>{{ cardHeader }}</h2>
+			<img :src="imgSrc" :alt="imgAlt" width="256" height="261" />
 		</div>
+		<h2>{{ cardHeader }}</h2>
+		<span>{{ cardSubheader }}</span>
+		<p>{{ cardContent }}</p>
+		<PrimaryButton buttonLabel="Click" />
 	</article>
 </template>
+
+<style lang="scss" scoped></style>
