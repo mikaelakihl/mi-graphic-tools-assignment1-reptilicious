57a8ed88102dc7aec1d090afee646205b07f7776
matildasoderhall <soderhall.matilda@gmail.com>
2025-03-07 11:50:20 +0100

    add productCard to ProductView and import imgs

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,3 +1,18 @@
+<script lang="ts" setup>
+import ProductCard from '../components/ProductCard.vue';
+import krokodileImageGreen from '../assets/images/krokodil_kobra_green.jpg';
+import krokodileImageBlue from '../assets/images/krokodil_kobra_blue.jpg';
+import krokodileImageRed from '../assets/images/krokodil_kobra_red.png';
+</script>
+
 <template>
 	<h1>Products</h1>
+
+	<ProductCard
+		:imgSrc="krokodileImageGreen"
+		:imgAlt="'A bunch of happy reptiles in a green jungle'"
+		:cardHeader="'Cuban Crocodiles'"
+		:cardSubHeader="'Sweet'"
+		:cardContent="'Our famous cola flavored crocodiles!'"
+	/>
 </template>
