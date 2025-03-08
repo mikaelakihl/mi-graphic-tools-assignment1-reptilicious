382090153cc49cb8b7f706377accb3ce14ce80d7
amandawiktorsson <amandawiktorsson@me.com>
2025-03-08 10:59:03 +0100

    menu to menubutton

--- src/components/atoms/MenuIcon.vue
+++ src/components/atoms/MenuIcon.vue
@@ -1,14 +1,29 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const menuOpen = ref(false);
+
+const toggleMenu = () => {
+	menuOpen.value = !menuOpen.value;
+};
+</script>
 
 <template>
-	<button class="menu-icon">
+	<button class="menu-icon" @click="toggleMenu">
 		<div class="lines">
-			<span class="line top-line"></span>
-			<span class="line middle-line"></span>
-			<span class="line bottom-line"></span>
+			<span class="line top-line" :class="{ open: menuOpen }"></span>
+			<span class="line middle-line" :class="{ open: menuOpen }"></span>
+			<span class="line bottom-line" :class="{ open: menuOpen }"></span>
 		</div>
-		<span class="menu-text">menu</span>
+		<span class="menu-text" :class="{ hidden: menuOpen }">menu</span>
 	</button>
+	<div class="menu-overlay" :class="{ open: menuOpen }">
+		<nav class="menu">
+			<a href="#">Home</a>
+			<a href="#">Products</a>
+			<a href="#">About Us</a>
+		</nav>
+	</div>
 </template>
 
 <style lang="scss" scoped>
@@ -57,12 +72,57 @@
 
 .menu-text {
 	font-size: 0.97rem;
-	font-weight: 400;
+	font-weight: lighter;
 	color: #ffe552;
 	position: absolute;
 	top: 20%;
 	left: 0;
 	transform: rotate(-1deg) translateY(0.1rem);
 	z-index: 1;
+}
+
+/* menu style */
+.menu-overlay {
+	position: fixed;
+	top: 1rem;
+	left: 1rem;
+	right: 1rem;
+	bottom: 1rem;
+	background: #056a55;
+	border-radius: 10px;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	transition: transform 0.5s ease-in-out;
+	transform: translateY(-100%);
+	z-index: 10;
+}
+
+/* menu open */
+.menu-overlay.open {
+	transform: translateY(0);
+}
+
+/* menu-links */
+.menu {
+	display: flex;
+	flex-direction: column;
+	gap: 4rem;
+}
+
+.menu a {
+	font-family: 'Spicy Rice', sans-serif;
+	font-size: 3rem;
+	color: #ffe552;
+	text-transform: uppercase;
+	text-decoration: none;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+
+.menu a:hover {
+	color: #fbae29;
 }
 </style>
