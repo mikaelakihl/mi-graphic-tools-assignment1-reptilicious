ccd311c4e3ec47bc0376ae2f45cf25ebd11f9dc8
matildasoderhall <soderhall.matilda@gmail.com>
2025-03-10 11:12:22 +0100

    auto format code

--- src/components/atoms/TertiaryButton.vue
+++ src/components/atoms/TertiaryButton.vue
@@ -1,30 +1,27 @@
 <script setup lang="ts">
-
 const props = defineProps<{
 	buttonLabel: string;
 	type?: 'button' | 'submit' | 'reset';
 }>();
-
 </script>
 
 <template>
-    <button :type="type">
-        {{ buttonLabel }}
-    </button>
+	<button :type="type">
+		{{ buttonLabel }}
+	</button>
 </template>
 
 <style lang="scss" scoped>
 button {
-
-    background-color: $forest-color;
-    color: $orange-color;
-    border: solid 3px $orange-color;
-    border-radius: $button-border-radius;
-    padding-inline: $small-margin;
-    font-family: $primary-font;
-    font-size: $h4-fontsize;
-    text-transform: uppercase;
-    filter: drop-shadow(rgba(0, 0, 0, 0.25) -4px 4px 4px);
+	background-color: $forest-color;
+	color: $orange-color;
+	border: solid 3px $orange-color;
+	border-radius: $button-border-radius;
+	padding-inline: $small-margin;
+	font-family: $primary-font;
+	font-size: $h4-fontsize;
+	text-transform: uppercase;
+	filter: drop-shadow(rgba(0, 0, 0, 0.25) -4px 4px 4px);
 }
 
 button:hover {
