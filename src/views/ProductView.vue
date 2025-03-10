b57bb3e50f45ea063062c01f7e29f66870e56eac
matildasoderhall <soderhall.matilda@gmail.com>
2025-03-10 10:43:13 +0100

    add grid to tablet and desktop

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -10,33 +10,32 @@ import krokodileImageRed from '../assets/images/krokodil_kobra_red.png';
 	<main>
 		<h1>Products</h1>
 		<SmallCard
+			class="small-card"
 			:cardHeader="'Taste the jungle'"
 			:cardSubheader="'Here you can find all our crazy flavors!'"
 			:cardContent="''"
 		/>
-		<div class="product-card-container">
-			<ProductCard
-				:imgSrc="krokodileImageGreen"
-				:imgAlt="'A bunch of happy reptiles in a green jungle'"
-				:cardHeader="'Cuban Crocodiles'"
-				:cardSubheader="'Sweet'"
-				:cardContent="'Our famous cola flavored crocodiles!'"
-			/>
-			<ProductCard
-				:imgSrc="krokodileImageRed"
-				:imgAlt="'A bunch of happy reptiles in a red jungle'"
-				:cardHeader="'King Cobras'"
-				:cardSubheader="'Sour'"
-				:cardContent="'Nothing beats the king!'"
-			/>
-			<ProductCard
-				:imgSrc="krokodileImageBlue"
-				:imgAlt="'A bunch of happy reptiles in a blue jungle'"
-				:cardHeader="'Lizard Sizzle'"
-				:cardSubheader="'Salty'"
-				:cardContent="'These lizards are sizzling!'"
-			/>
-		</div>
+		<ProductCard
+			:imgSrc="krokodileImageGreen"
+			:imgAlt="'A bunch of happy reptiles in a green jungle'"
+			:cardHeader="'Cuban Crocodiles'"
+			:cardSubheader="'Sweet'"
+			:cardContent="'Our famous cola flavored crocodiles!'"
+		/>
+		<ProductCard
+			:imgSrc="krokodileImageRed"
+			:imgAlt="'A bunch of happy reptiles in a red jungle'"
+			:cardHeader="'King Cobras'"
+			:cardSubheader="'Sour'"
+			:cardContent="'Nothing beats the king!'"
+		/>
+		<ProductCard
+			:imgSrc="krokodileImageBlue"
+			:imgAlt="'A bunch of happy reptiles in a blue jungle'"
+			:cardHeader="'Lizard Sizzle'"
+			:cardSubheader="'Salty'"
+			:cardContent="'These lizards are sizzling!'"
+		/>
 	</main>
 </template>
 
@@ -58,24 +57,43 @@ article {
 	min-width: 100%;
 }
 
-.product-card-container {
-	display: flex;
-	flex-direction: column;
-	gap: $small-margin;
-}
-
 @media screen and (min-width: $tablet-size) {
 	main {
 		margin: $large-margin;
+		display: grid;
+		grid-template-columns: repeat(6, 1fr);
+		column-gap: $small-margin;
 	}
-
-	.product-card-container {
-		flex-direction: unset;
-		flex-wrap: wrap;
+	h1 {
+		grid-column: span 6;
+		text-align: center;
+	}
+	.small-card {
+		grid-column: span 6;
 	}
 
 	.product-card {
-		min-width: 48%;
+		grid-column: span 3;
+	}
+}
+
+@media screen and (min-width: $desktop-size) {
+	main {
+		display: grid;
+		grid-template-columns: repeat(12, 1fr);
+		column-gap: $large-margin;
+	}
+
+	h1 {
+		grid-column: span 12;
+	}
+
+	.small-card {
+		grid-column: span 12;
+	}
+
+	.product-card {
+		grid-column: span 4;
 	}
 }
 </style>
